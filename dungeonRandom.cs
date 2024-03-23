c072b697ce7098a3a83cb9d49f6593c677a967fb
sBecker-atown <admin@sebastianbeckerart.de>
2024-03-23 15:16:51 +0100

    Changed Globals to Set of enums, changed plan for further steps to dungeonRandomV2.txt

--- dungeonRandom.cs
+++ dungeonRandom.cs
@@ -1,178 +1,174 @@
 ﻿using System;
 using System.ComponentModel;
 
-// Possible Values a space on grid can have
-// Wall, Door, Floor, Corridor, Blank
-// Wall = 1, Door = 2, Floor = 3, Corridor = 4, Blank = 0;
-
 namespace DungeonRandom
 {
-    static class Globals
+    enum Direction
     {
-        public static int canvasWidth = 80;
-        public static int canvasHeight = 20;
-        public static int max = 10;
-        public static int min = 3;
-        
-        // Floor tiles
-        public static int wall = 1;
-        public static int door = 2;
-        public static int floor = 3;
-        public static int corridor = 4;
-        public static int blank = 0;
-
-        // Directions
-        public static int north = 1;
-        public static int east = 2;
-        public static int south = 3;
-        public static int west = 4;
+        north,
+        east,
+        south,
+        west,
+    }
+    enum Size
+    {
+        canvasW = 80,
+        canvasH = 20,
+        maxRoomSize = 10,
+        minRoomSize = 3,
+    }
+    enum Tile
+    {        
+        blank,
+        wall,
+        door,
+        floor,
+        corridor,
     }
     class Program
     {
         static void Main(string[] args)
         {
             // Set grid to Blank Space
-            int[,] grid = new int[Globals.canvasHeight, Globals.canvasWidth];
-            for (int i = 0; i < Globals.canvasHeight; i++)
+            int[,] grid = new int[(int)Size.canvasH, (int)Size.canvasW];
+            for (int i = 0; i < (int)Size.canvasH; i++)
             {
-                for (int j = 0; j < Globals.canvasWidth; j++)
+                for (int j = 0; j < (int)Size.canvasW; j++)
                 {
-                    grid[i,j] = Globals.blank;
-                    // Console.Write("0");
+                    grid[i,j] = (int)Tile.blank;
                 }
-                // Console.Write("\n");
             }
 
             // Generate a random Room 
-            generateRoom(Globals.max, Globals.min);
+            GenerateRoom((int)Size.maxRoomSize, (int)Size.minRoomSize);
         }
-        static void generateRoom(int max, int min)
+        static void GenerateRoom(int max, int min)
         {
-            int[,] memGrid = new int[Globals.canvasHeight, Globals.canvasWidth];
-            // Generate random Numbers for temporary room array
+            int[,] memGrid = new int[(int)Size.canvasH, (int)Size.canvasW];
+            // Generate random Numbers for temporary room array.
             var rand = new Random();
-            int randomWidth = rand.Next(min, max) ;
-            int randomHeight = rand.Next(min, max);
+            int randomWidth = rand.Next((int)Size.minRoomSize, (int)Size.maxRoomSize) ;
+            int randomHeight = rand.Next((int)Size.minRoomSize, (int)Size.maxRoomSize);
 
-            // Create temporary room array
+            // Create temporary room array.
             int[,] room = new int[randomHeight, randomWidth];
 
-            // Draw Walls
-            generateWalls(randomHeight, randomWidth, room);
+            // Draw Walls.
+            GenerateWalls(randomHeight, randomWidth, room);
 
-            generateDoors(randomHeight, randomWidth, room);
+            GenerateDoors(randomHeight, randomWidth, room);
 
-            // Write Room into center of memGrid
+            // Write Room into center of memGrid.
             for (int i = 0; i < randomHeight; i++)
             {
                 for (int j = 0; j < randomWidth; j++)
                 {
-                    // RUNDEN wäre gut hier
-                    memGrid[(Globals.canvasHeight / 2) - (randomHeight / 2) + i, (Globals.canvasWidth / 2) - (randomHeight / 2) + j] = room[i,j];
+                    // RUNDEN wäre gut hier.
+                    memGrid[((int)Size.canvasH / 2) - (randomHeight / 2) + i, ((int)Size.canvasW / 2) - (randomHeight / 2) + j] = room[i,j];
                 }
             }
-            // DEBUG Output of random room
-            printDungeon(Globals.canvasHeight, Globals.canvasWidth, memGrid);
+            // DEBUG Output of random room.
+            PrintDungeon((int)Size.canvasH, (int)Size.canvasW, memGrid);
         }
-        static int[,] generateWalls(int height, int width, int[,] room)
+        static int[,] GenerateWalls(int height, int width, int[,] room)
         {
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    // Draw northern and southern wall
+                    // Draw northern and southern wall.
                     if (i == 0 || i == height - 1)
                     {
-                        room[i,j] = Globals.wall;
+                        room[i,j] = (int)Tile.wall;
                     }
-                    // Draw eastern and western wall
+                    // Draw eastern and western wall.
                     else if (j == 0 || j == width - 1)
                     {
-                        room[i,j] = Globals.wall;
+                        room[i,j] = (int)Tile.wall;
                     }
-                    // Draw inside of room
+                    // Draw inside of room.
                     else 
                     {
-                        room[i,j] = Globals.floor;
+                        room[i,j] = (int)Tile.floor;
                     }
                 }
             }
             return room;
         }
-        static int[,] generateDoors(int height, int width, int[,]room)
+        static int[,] GenerateDoors(int height, int width, int[,]room)
         {
-            // Go to random wall
-            // North = 1, East = 2, South = 3, West = 4
+            // Go to random wall.
+            // North = 1, East = 2, South = 3, West = 4.
             var rand = new Random();
-            int pickedWall = rand.Next(Globals.north, Globals.west + 1);
+            int pickedWall = rand.Next((int)Direction.north, (int)Direction.west + 1);
 
-            // Check if wall has door
-            if (checkDoor(pickedWall, height, width, room) == false)
+            // Check if wall has door.
+            if (CheckDoor(pickedWall, height, width, room) == false)
             {
-                // Pick spot on the wall
-                if (pickedWall == Globals.north)
+                // Pick spot on the wall.
+                if (pickedWall == (int)Direction.north)
                 {
                     int pickedSpot = rand.Next(1, width - 2);
-                    room[0,pickedSpot] = Globals.door;
+                    room[0,pickedSpot] = (int)Tile.door;
                 }
-                else if (pickedWall == Globals.east)
+                else if (pickedWall == (int)Direction.east)
                 {
                     int pickedSpot = rand.Next(1, height - 2);
-                    room[pickedSpot, width - 1] = Globals.door;
+                    room[pickedSpot, width - 1] = (int)Tile.door;
                 }
-                else if (pickedWall == Globals.south)
+                else if (pickedWall == (int)Direction.south)
                 {
                     int pickedSpot = rand.Next(1, width - 2);
-                    room[height - 1, pickedSpot] = Globals.door;
+                    room[height - 1, pickedSpot] = (int)Tile.door;
                 }
-                else if (pickedWall == Globals.west)
+                else if (pickedWall == (int)Direction.west)
                 {
                     int pickedSpot = rand.Next(1, height - 2);
-                    room[pickedSpot, 0] = Globals.door;
+                    room[pickedSpot, 0] = (int)Tile.door;
                 }
             }
             return room;
         }
-        static bool checkDoor(int wall, int height, int width, int[,] room)
+        static bool CheckDoor(int wall, int height, int width, int[,] room)
        {
-            if (wall == Globals.north)
+            if (wall == (int)Direction.north)
             {
                 for (int i = 1; i < width - 2; i++)
                 {
-                    if (room[0, i] == Globals.door)
+                    if (room[0, i] == (int)Tile.door)
                     {
                         return true;
                     }
                 }
                 return false;
             }  
-            else if (wall == Globals.east)
+            else if (wall == (int)Direction.east)
             {
                 for (int i = 1; i < height - 2; i++)
                 {
-                    if (room[i, width - 1] == Globals.door)
+                    if (room[i, width - 1] == (int)Tile.door)
                     {
                         return true;
                     }
                 }
                 return false;
             }
-            else if (wall == Globals.south)
+            else if (wall == (int)Direction.south)
             {
                 for (int i = 1; i < width - 2; i++)
                 {
-                    if (room[height - 1,i] == Globals.door)
+                    if (room[height - 1,i] == (int)Tile.door)
                     {
                         return true;
                     }
                 }
                 return false;
             }  
-            else if (wall == Globals.west)
+            else if (wall == (int)Direction.west)
             {
                 for (int i = 1; i < height - 2; i++)
                 {
-                    if (room[i, 0] == Globals.door)
+                    if (room[i, 0] == (int)Tile.door)
                     {
                         return true;
                     }
@@ -184,29 +180,29 @@ namespace DungeonRandom
                 return false;
             }     
        } 
-        static void printDungeon(int height, int width, int[,] room)
+        static void PrintDungeon(int height, int width, int[,] room)
         {
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    if (room[i,j] == Globals.wall)
+                    if (room[i,j] == (int)Tile.wall)
                     {
                         Console.Write("#");
                     }
-                    else if (room[i,j] == Globals.door)
+                    else if (room[i,j] == (int)Tile.door)
                     {
                         Console.Write(".");
                     }
-                    else if (room[i,j] == Globals.floor)
+                    else if (room[i,j] == (int)Tile.floor)
                     {
                         Console.Write(".");
                     }
-                    else if (room[i,j] == Globals.corridor)
+                    else if (room[i,j] == (int)Tile.corridor)
                     {
                         Console.Write(".");
                     }
-                    else if (room[i,j] == Globals.blank)
+                    else if (room[i,j] == (int)Tile.blank)
                     {
                         Console.Write(" ");
                     }
